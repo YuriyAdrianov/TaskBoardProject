585702aa94c3ac8239df07959098066fa515391b
YuriyAdrianov <shwainroug@gmail.com>
2022-03-10 19:24:05 +0400

    worker mapper was filled

--- src/main/java/com/yuriyadr/taskboard/service/mapper/WorkerMapper.java
+++ src/main/java/com/yuriyadr/taskboard/service/mapper/WorkerMapper.java
@@ -1,7 +1,22 @@
 package com.yuriyadr.taskboard.service.mapper;
 
+import com.yuriyadr.taskboard.dto.WorkerDto;
+import com.yuriyadr.taskboard.entity.Worker;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WorkerMapper {
+    public WorkerDto toDto(Worker worker) {
+        WorkerDto workerDto = new WorkerDto();
+        workerDto.setName(worker.getName());
+        workerDto.setActivity(worker.getActivity());
+        return workerDto;
+    }
+
+    public Worker toEntity(WorkerDto workerDto) {
+        Worker worker = new Worker();
+        worker.setName(workerDto.getName());
+        worker.setActivity(workerDto.getActivity());
+        return worker;
+    }
 }
