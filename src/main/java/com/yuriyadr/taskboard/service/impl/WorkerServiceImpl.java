a5245470e290b55935f50eb87255709f65010c9d
YuriyAdrianov <shwainroug@gmail.com>
2022-03-10 19:19:12 +0400

    just implement methods. still need to fill

--- src/main/java/com/yuriyadr/taskboard/service/impl/WorkerServiceImpl.java
+++ src/main/java/com/yuriyadr/taskboard/service/impl/WorkerServiceImpl.java
@@ -1,8 +1,35 @@
 package com.yuriyadr.taskboard.service.impl;
 
+import com.yuriyadr.taskboard.dto.WorkerDto;
 import com.yuriyadr.taskboard.service.WorkerService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class WorkerServiceImpl implements WorkerService {
+    @Override
+    public List<WorkerDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public WorkerDto findById() {
+        return null;
+    }
+
+    @Override
+    public WorkerDto create(WorkerDto workerDto) {
+        return null;
+    }
+
+    @Override
+    public WorkerDto update(WorkerDto workerDto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
